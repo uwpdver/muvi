836e4d426a27405be7ebcd20f7580c0408536c34
lzh <1119548217@qq.com>
2020-11-06 09:51:48 +0800

    初始化

---
+++ pages/home/home.js
@@ -0,0 +1,213 @@
+Page({
+  data: {
+    name: "小李",
+    items: [
+      {
+        id: 1,
+        name: '无间道',
+        alias: '無間道',
+        publishedYear: '2002',
+        desc: '1991年，香港黑帮三合会会员刘健明（刘德华）听从老大韩琛（曾志伟）的吩咐，加入警察部队成为黑帮卧底，韩琛许诺刘健明会帮其在七年后晋升为见习督察。1992年，警察训练学校优秀学员陈永仁（梁朝伟）被上级要求深入到三合会做卧底，终极目标是成为韩琛身边的红人。2002年，两人都不负重望，也都身背重压，刘健明渐想成为一个真正的好人，陈永仁则盼着尽快回归警察身份。重案组从陈永仁手中获悉一批毒品交易情报，锁紧目标人物韩琛，没料情报被刘健明泄出，双方行动均告失败。但此事将双方均有卧底的事实暴露，引发双方高层清除内鬼的决心。命运迥异又相似的刘健明和陈永仁开始在无间道的旅程中接受严峻考验。',
+        directs: [
+          {
+            id: '1',
+            name: '刘伟强'
+          },
+          {
+            id: '2',
+            name: '麦兆辉'
+          }
+        ],
+        actors: [
+          {
+            id: '1',
+            name: '刘德华'
+          },
+          {
+            id: '2',
+            name: '梁朝伟'
+          },
+          {
+            id: '3',
+            name: '黄秋生'
+          },
+          {
+            id: '4',
+            name: '曾志伟'
+          },
+          {
+            id: '5',
+            name: '郑秀文'
+          },
+        ],
+        categorys: [
+          {
+            id: 1,
+            name: '剧情'
+          },
+          {
+            id: 1,
+            name: '悬疑'
+          },
+          {
+            id: 1,
+            name: '犯罪'
+          }
+        ],
+        cover: 'https://img2.doubanio.com/view/photo/s_ratio_poster/public/p2564556863.webp',
+        images: [
+          'https://img9.doubanio.com/view/photo/raw/public/p1182116984.jpg',
+          'https://img1.doubanio.com/view/photo/raw/public/p2581669417.jpg',
+          'https://img3.doubanio.com/view/photo/raw/public/p2588860640.jpg'
+        ]
+      },
+      {
+        id: 2,
+        name: '无间道2',
+        alias: '無間道II',
+        publishedYear: '2003',
+        desc: '1991年，香港黑帮三合会会员刘健明（刘德华）听从老大韩琛（曾志伟）的吩咐，加入警察部队成为黑帮卧底，韩琛许诺刘健明会帮其在七年后晋升为见习督察。1992年，警察训练学校优秀学员陈永仁（梁朝伟）被上级要求深入到三合会做卧底，终极目标是成为韩琛身边的红人。2002年，两人都不负重望，也都身背重压，刘健明渐想成为一个真正的好人，陈永仁则盼着尽快回归警察身份。重案组从陈永仁手中获悉一批毒品交易情报，锁紧目标人物韩琛，没料情报被刘健明泄出，双方行动均告失败。但此事将双方均有卧底的事实暴露，引发双方高层清除内鬼的决心。命运迥异又相似的刘健明和陈永仁开始在无间道的旅程中接受严峻考验。',
+        directs: [
+          {
+            id: '1',
+            name: '刘伟强'
+          },
+          {
+            id: '2',
+            name: '麦兆辉'
+          }
+        ],
+        actors: [
+          {
+            id: '1',
+            name: '刘德华'
+          },
+          {
+            id: '2',
+            name: '梁朝伟'
+          },
+          {
+            id: '3',
+            name: '黄秋生'
+          },
+          {
+            id: '4',
+            name: '曾志伟'
+          }
+        ],
+        categorys: [
+          {
+            id: 1,
+            name: '剧情'
+          },
+          {
+            id: 1,
+            name: '悬疑'
+          },
+          {
+            id: 1,
+            name: '犯罪'
+          }
+        ],
+        cover: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p958008320.webp',
+        images: [
+          'https://img9.doubanio.com/view/photo/raw/public/p1182116984.jpg',
+          'https://img1.doubanio.com/view/photo/raw/public/p2581669417.jpg',
+          'https://img3.doubanio.com/view/photo/raw/public/p2588860640.jpg'
+        ]
+      },
+      {
+        id: 3,
+        name: '无间道3：终极无间',
+        alias: '無間道III 終極無間',
+        publishedYear: '2003',
+        desc: '1991年，香港黑帮三合会会员刘健明（刘德华）听从老大韩琛（曾志伟）的吩咐，加入警察部队成为黑帮卧底，韩琛许诺刘健明会帮其在七年后晋升为见习督察。1992年，警察训练学校优秀学员陈永仁（梁朝伟）被上级要求深入到三合会做卧底，终极目标是成为韩琛身边的红人。2002年，两人都不负重望，也都身背重压，刘健明渐想成为一个真正的好人，陈永仁则盼着尽快回归警察身份。重案组从陈永仁手中获悉一批毒品交易情报，锁紧目标人物韩琛，没料情报被刘健明泄出，双方行动均告失败。但此事将双方均有卧底的事实暴露，引发双方高层清除内鬼的决心。命运迥异又相似的刘健明和陈永仁开始在无间道的旅程中接受严峻考验。',
+        directs: [
+          {
+            id: '1',
+            name: '刘伟强'
+          },
+          {
+            id: '2',
+            name: '麦兆辉'
+          }
+        ],
+        actors: [
+          {
+            id: '1',
+            name: '刘德华'
+          },
+          {
+            id: '2',
+            name: '梁朝伟'
+          },
+          {
+            id: '3',
+            name: '黄秋生'
+          },
+          {
+            id: '4',
+            name: '曾志伟'
+          }
+        ],
+        categorys: [
+          {
+            id: 1,
+            name: '剧情'
+          },
+          {
+            id: 1,
+            name: '悬疑'
+          },
+          {
+            id: 1,
+            name: '犯罪'
+          }
+        ],
+        cover: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2527603461.webp',
+        images: [
+          'https://img9.doubanio.com/view/photo/raw/public/p1182116984.jpg',
+          'https://img1.doubanio.com/view/photo/raw/public/p2581669417.jpg',
+          'https://img3.doubanio.com/view/photo/raw/public/p2588860640.jpg'
+        ]
+      }
+    ]
+  },
+  onShow: function () {
+    console.log(this.route)
+  },
+  filmTap(event) {
+    console.log(event);
+    console.log(event.target.dataset.id)
+    wx.navigateTo({
+      url: '../detail/detail',
+      success: (result) => {
+        console.log(result);
+      },
+      fail: (error) => {
+        console.log(error);
+      },
+      complete: () => { }
+    });
+  },
+  buttonHandler(event) {
+    wx.navigateTo({
+      url: '../detail/detail',
+      success: (result) => {
+        console.log(result);
+      },
+      fail: (error) => {
+        console.log(error);
+      },
+      complete: () => { }
+    });
+    // this.setData({
+    //   name: "Puzz"
+    // }, function(){
+    //   // wx.showToast({
+    //   //   title: '完成数据修改',
+    //   //   duration: 700
+    //   // })
+    // })
+  }
+});
