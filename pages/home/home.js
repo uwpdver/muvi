4ce556985699e0f9df4873ab6cb407f282b751f5
lzh <1119548217@qq.com>
2020-11-12 15:19:25 +0800

    feat: 封装网络请求方法;

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,7 +1,21 @@
-const app = getApp(); 
+import request from '../../lib/request';
+
+const app = getApp();
 
 Page({
   data: {
     items: app.globalData.items
+  },
+  onLoad: function (options) {
+    wx.showToast({ title: "页面加载完成" });
+    request.get('lessons/lesson-pack/all-list')
+      .then(
+        res => {
+          console.log(res)
+        },
+        error => {
+          console.log(error)
+        }
+      )
   }
 });
